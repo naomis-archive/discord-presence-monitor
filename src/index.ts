b548e7cc6d873d06c24537fbdc41a007e7ad76a8
Naomi Carrigan <nhcarrigan@gmail.com>
2022-03-07 18:24:59 -0800

    feat: basic bot code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Naomi!!!");
+import { Client } from "discord.js";
+
+import { logHandler } from "./utils/logHandler";
+
+(async () => {
+  const bot = new Client({ intents: ["GUILDS", "GUILD_PRESENCES"] });
+
+  bot.on("ready", () => {
+    logHandler.log("debug", "Discord bot connected!");
+    bot.user?.setActivity("Naomi's presence.", { type: "WATCHING" });
+  });
+
+  await bot.login(process.env.BOT_TOKEN);
+})();
